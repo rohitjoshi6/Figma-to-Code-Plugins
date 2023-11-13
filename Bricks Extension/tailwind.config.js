23029827c523c1a9c9e5a15c8d291f8b0535348d
Rohit_Joshi <rohitjoshi632001@gmail.com>
2023-11-13 17:32:55 +0530

    Init

---
+++ Bricks Extension/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      fontFamily: {
+        "plus-jakarta-sans": "'Plus Jakarta Sans', sans-serif",
+      },
+      extend: {
+        backgroundImage: {
+          SvgAsset13: "url(/assets/SvgAsset13.svg)",
+          SvgAsset18: "url(/assets/SvgAsset18.svg)",
+          SvgAsset20: "url(/assets/SvgAsset20.svg)",
+          SvgAsset23: "url(/assets/SvgAsset23.svg)",
+          SvgAsset26: "url(/assets/SvgAsset26.svg)",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
